470d2e9c2f04456f00773a6969d849e50da31e35
MAamer28 <aamer.moe@Gmail.com>
2023-05-18 17:32:24 -0400

    Submission 1
    The coding for the initial submission given the May 18 deadline. I will come back to this to fix the "Average Change" calculation as it is currently providing an incorrect value.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,14 @@
+#Modules are imported as needed.
 import os
 import csv
 
+#The CSV file is extracted via a path defined as relative to the main "python-challenge" repository.
 data_path = os.path.join("PyBank", "Resources","budget_data.csv")
 
+#A path is created for the output text file that will be created at the end of the code.
 text_output = os.path.join("PyBank", "finanalysis_output.txt")
 
+#The variables that will be used to calculate the results are defined here.
 total_months = 0
 net_total = 0
 monthly_rev = []
@@ -16,20 +20,29 @@ max_gain = ["", 0]
 rev_rate = []
 rev_average = 0
 
+#The CSV file containing the raw data is extracted and read as a dictionary, with the headers automatically read as keys.
 with open(data_path, newline= '') as csvfile:
     budget_csv = csv.DictReader(csvfile)
 
     for row in budget_csv:
-
+        #Firstly, the total number of months in the data file is calculated via an iterative loop by adding +1 to the total_months variable for each row of data (minus the header row) in the CSV file.
         total_months += 1
 
+        #The net total of profits or losses is calculated via an iterative loop where each value in the 'Profit/Losses' column is added to the previous value to determine net total.
         net_total = net_total + int(row['Profit/Losses'])
 
+        #Here, I am getting the wrong value compared to Boot Camp Spot. In calculating the rate of change, I am trying to add the sum of the values of a row and the previous row to the "rev_rate" list.
+        #In other words, the below functions are meant to iterate through the column and calculate x = February - January and so on. However, the average rate of change this results in is $4448.13. 
+        #The Challenge Module's page on Boot Camp Spot reflects a correct result of $-8311.11. There is something wrong here that I have been trying to fix to no avail. 
+        #If these comments are still here when the assignment is corrected, it is because I ran out of time before the deadline. My aim will be to resubmit the assignment once I work on this issue further over the weekend.
         rev_change = float(row['Profit/Losses']) - prevmonth_rev
         prevmonth_rev = float(row["Profit/Losses"])
         rev_rate = [rev_change] + rev_rate
         monthly_change = [monthly_change] + [row["Date"]]
 
+        rev_average = sum(rev_rate)/len(rev_rate)
+
+        #Here, we determine the months each with the greatest increase in profits and greatest decrease in profits.
         if rev_change > max_gain[1]:
             max_gain[1] = rev_change
             max_gain[0] = row['Date']
@@ -38,15 +51,15 @@ with open(data_path, newline= '') as csvfile:
             max_loss[1] = rev_change
             max_loss[0] = row['Date']
     
-    rev_average = sum(rev_rate)/len(rev_rate)
-
+    
+    #The results are printed to the terminal.
     print("Total Months: " + str(total_months))
     print("Total: $" + str(net_total))
     print("Average Change: $" + str(rev_average))
     print("Greatest Increase in Profits: " + str(max_gain [0]) + "($" + str(max_gain[1]) + ")")
     print("Greatest Decrease in Profits: " + str(max_loss[0]) + "($" + str(max_loss[1]) + ")") 
 
-
+#The results are then written into a text file to reflect the appearance seen on the Boot Camp Spot page.
 with open(text_output, "w") as file:
     file.write("Financial Analysis\n")
     file.write("Total Months: " + str(total_months) + "\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,26 @@
+#First, we import the modules we will use.
 import os
 import csv
 
+#The file path is defined relative to the main "python-challenge" repository.
 file_path = os.path.join("PyPoll", "Resources", "election_data.csv")
 
+#We also define the path for the text file that will be created at the end of the assignment.
 text_output = os.path.join("PyPoll", "poll_output.txt")
 
+#We define some variables, namely the candidate list to be a dictionary and a "line-break" variable that we will use to create the breaks to clean up the presentation of the data at the end.
 candidate_list = {}
 linebreak = "-------------------------"
 
+#The CSV file is read by Python and the headers are fixed.
 with open(file_path, encoding="utf-8") as csvfile:
     poll_csv = csv.reader(csvfile, delimiter=",")
     poll_header = next(poll_csv)
 
+    #We calculate the total number of votes in the poll. Firstly, we set the vote total as 0.
+    #Then, we create an iterative loop where every row adds +1 to the vote total.
+    #Candidates are defined as being in the column corresponding to index = 2. 
+    #Each candidate receives +1 vote for every row in which their name is listed in the "Candidates" column, i.e. Index = 2 Column.
     vote_total = 0
     for row in poll_csv:
         vote_total += 1
@@ -21,12 +30,16 @@ with open(file_path, encoding="utf-8") as csvfile:
         else:
             candidate_list[candidate] = 1
 
+    #The percentage of votes for each candidate are calculated as the number of votes for a given candidate divided by the total number of votes, multiplied by 100.
+    #Note that the parameter "3" here in the "round" function indicates each % value will be rounded to 3 decimal places.
     candidate_list["Pct Stockham"] = round((candidate_list["Charles Casper Stockham"]/vote_total)*100, 3)
     candidate_list["Pct DeGette"] = round((candidate_list["Diana DeGette"]/vote_total)*100, 3)
     candidate_list["Pct Doane"] = round((candidate_list["Raymon Anthony Doane"]/vote_total)*100, 3)
 
+    #The winner is determined as the candidate with the highest number of votes, or the maximum nunber in the list aggregating the number of votes of each candidate.
     winner = max(candidate_list, key=candidate_list.get)
 
+    #The results are printed and tabulated as follows.
     print("Total Votes: " + str(vote_total))
     print(linebreak)
     print("Charles Casper Stockham: " + str(candidate_list["Pct Stockham"]) + "% (" + str(candidate_list["Charles Casper Stockham"]) +")")
@@ -36,6 +49,7 @@ with open(file_path, encoding="utf-8") as csvfile:
     print("Winner: " + winner)
     print(linebreak)
 
+#The output is then aggregated into a text file as follows, with the text file created in the path defined above.
 with open(text_output, "w") as file:
     file.write("Election Results" + "\n")
     file.write(linebreak + "\n")
